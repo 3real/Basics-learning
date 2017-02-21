b7b6fc0e64676a321b564e77e2869a7f6a79b272
3real <mikegrahameverett@gmail.com>
2017-02-21 23:26:14 +0000

    Humanoid,gamemanger, player, and zombie scripts added

---
+++ gamemanager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        zombie zombie = new zombie();
+        player player = new player();
+        
+
+        player.TakeDamage(zombie.Attack());
+        player.TakeDamage(zombie.AcidPukeAttack());
+
+        player.Die();
+
+        zombie[] zombies = new zombie[100];
+
+        for (int i = 0; i < zombies.Length; i++)
+        {
+            zombies[i] = new zombie();
+            Debug.Log("Creating Zombie #:" + i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
